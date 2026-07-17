6ccab8c03837525c475a8213c02705768b680bea
Jairo Salinas <jairosalinas.g15@gmail.com>
2026-07-17 13:23:56 -0400

    Conectar frontend con backend Render

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const api = axios.create({
 
-    baseURL:"https://plataforma-vacaciones-api.onrender.com"
+    baseURL:"https://plataforma-vacaciones-api.onrender.com/api"
 
 });
 
